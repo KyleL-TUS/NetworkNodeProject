337c1d79a5bc2ad1e4fdf735c4c48e6eb9a523d9
Kyle Leahy <A00264102@student.tus.ie>
2024-05-02 19:09:33 +0100

    node controller

---
+++ src/main/java/org/ericsson/miniproject/NetworkNodeController.java
@@ -0,0 +1,55 @@
+package org.ericsson.miniproject;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api-v1/nodes")
+public class NetworkNodeController {
+
+    private final NetworkNodeService networkNodeService;
+
+    public NetworkNodeController(NetworkNodeService networkNodeService) {
+        this.networkNodeService = networkNodeService;
+    }
+
+    // REST endpoints for CRUD operations
+    @PostMapping
+    public ResponseEntity<ResponseMsg>  addNode(@RequestBody NetworkNode node) {
+        return ResponseEntity.ok().body(networkNodeService.addNode(node));
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<NetworkNode>> getAllNodes() {
+        // ResponseEntity
+        return ResponseEntity.ok().body(networkNodeService.getAllNodes());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NetworkNode> getNodeById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(networkNodeService.getNodeById(Math.toIntExact(id)));
+    }
+
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NetworkNode> updateNode(@PathVariable("id") Long id, @RequestBody NetworkNode updatedNode) {
+
+        return ResponseEntity.ok().body(networkNodeService.updateNode(Math.toIntExact(id), updatedNode));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<NetworkNode>  deleteNode(@PathVariable("id") Long id) {
+
+        //return ResponseEntity.ok().body(networkNodeService.deleteNode(Math.toIntExact(id)));
+        return null;
+    }
+
+}
